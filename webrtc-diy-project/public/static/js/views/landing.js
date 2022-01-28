b6c7c0f9fd4973e49fc2663e9596df888cf7eac4
Sofianossnahoj <70485216+Sofianossnahoj@users.noreply.github.com>
2022-01-28 14:39:14 +0100

    update

--- webrtc-diy-project/public/static/js/views/landing.js
+++ webrtc-diy-project/public/static/js/views/landing.js
@@ -9,17 +9,18 @@ export default class extends abstractView {
   async getHtml() {
     return `
    <main class="landing">
-    <section class="landing__card"> 
-      <h1>Welcome to this share application</h1>
-      <article class="landing__form">
-        <h3> Please enter your name </h3>
-          <input class="landing__input" type="text" id="enterName" />
-          <button class="button" id="nameOKBtn" >Add your name</button>
-          <p id="alertName"></p>
-          <br>
-          <a id="connectCondition" href="/createRoom" data-link>Connect </a> 
-        </article>
-    </section>
+    <aside class="landing__card"> 
+      <h1>Welcome <br> to this share application!</h1>
+    </aside>
+    <article class="landing__form">
+      <h3 id="registerInfo"> Please enter your name! </h3>
+        <input autocomplete="off" class="landing__input" placeholder="Name" type="text" id="enterName" />
+        <button class="button" id="nameOKBtn" >Add your name</button>
+        <p id="alertName"></p>
+        <br>
+        <a id="connectCondition" href="/createRoom" data-link>Connect </a> 
+      </article>
+    
    </main>
     `;
   }
